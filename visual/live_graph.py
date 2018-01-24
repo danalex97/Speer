33504da2a4844ea78ce045abfc2dc9b2cff65694
ad5915 <dan.alex97@yahoo.com>
2018-01-24 23:29:44 +0000

    Made a simple graph animation. This can be used for visualisation afterwards.

--- visual/live_graph.py
+++ visual/live_graph.py
@@ -1,47 +1,8 @@
-# import networkx as nx
-# import matplotlib.animation as animation
-# import matplotlib.pyplot as plt
-# import random
-# import time
-#
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-#
-# # Graph initialization
-# G = nx.Graph()
-# G.add_nodes_from([1, 2, 3, 4, 5, 6, 7, 8, 9])
-#
-# all_edges = [(1,2), (3,4), (2,5), (4,5), (6,7), (8,9), (4,7), (1,7), (3,5), (2,7), (5,8), (2,9), (5,7)]
-# G.add_edges_from(all_edges)
-#
-# # draw and show it
-# ax.relim()
-# ax.autoscale_view(True,True,True)
-# nx.draw(G, ax=ax)
-# plt.show(block=False)
-#
-# # time.sleep(1)
-#
-# edges = all_edges
-# while True:
-#     try:
-#         G.remove_edges_from(edges)
-#
-#         sample = random.sample(range(len(all_edges)), 8)
-#         edges = [all_edges[i] for i in sample]
-#
-#         G.add_edges_from(edges)
-#
-#         nx.draw(G, ax=ax)
-#
-#         time.sleep(1)
-#     except KeyboardInterrupt:
-#         break
-
 import random
 import pylab
 from matplotlib.pyplot import pause
 import networkx as nx
+
 pylab.ion()
 
 graph = nx.Graph()
@@ -58,11 +19,15 @@ def get_fig(fig):
     nx.draw(graph)
     return fig
 
-num_plots = 50;
 pylab.show()
 
 fig = pylab.figure()
 
-for i in range(num_plots):
-    fig = get_fig(fig)
-    pause(0.1)
+while True:
+    try:
+        fig.clf()
+        fig = get_fig(fig)
+        pause(0.1)
+
+    except KeyboardInterrupt:
+        break
