8cbc24d6a9c108acad193da398382acee213d794
ad5915 <dan.alex97@yahoo.com>
2018-02-15 18:56:49 +0000

    Added simple tests for packet and connection.

---
+++ underlay/connection_test.go
@@ -0,0 +1,16 @@
+package underlay
+
+import (
+  "testing"
+)
+
+func TestStaticConnectionLatencyRemainsConstant(t *testing.T) {
+  r := NewShortestPathRouter()
+  l := 10
+  c := NewStaticConnection(l, r)
+
+  assertEqual(t, c.Router(), r)
+  for i := 0; i < 10; i++ {
+    assertEqual(t, c.Latency(), l)
+  }
+}
---
+++ underlay/packet_test.go
@@ -0,0 +1,14 @@
+package underlay
+
+import (
+  "testing"
+)
+
+func TestNewPacket(t *testing.T) {
+  r := NewShortestPathRouter()
+  p := NewPacket(r, r, nil)
+
+  assertEqual(t, p.Src(), r)
+  assertEqual(t, p.Dest(), r)
+  assertEqual(t, p.Payload(), nil)
+}
