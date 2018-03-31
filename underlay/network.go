17e9565c95490647d53e9380658dc68f897c1713
Alexandru Dan <dan.alex97@yahoo.com>
2018-03-31 10:44:19 +0100

    Added explaination on generating an internetwork.

--- underlay/network.go
+++ underlay/network.go
@@ -13,6 +13,28 @@ func (n *Network) RandomRouter() Router {
   return n.Routers[rand.Intn(len(n.Routers))]
 }
 
+/* Generates a 2-level transit-stub topology following the paper:
+ Zegura E., Calvert K. and Bhattacharjee S. How to model an internetwork. In INFOCOM’96 (1996)
+
+  T  = number of tranasit domains
+  Nt = avg. nodes per transit domain
+  S  = number of stub domains
+  Ns = avg. nodes per stub domain
+
+  The transit domains represent the backbone, whereas a stub is attached to a
+  node in the backbone.
+
+  The graph is generated as follows:
+  1. Generate transit domain graph
+  2. Generate graph from tranasit domain
+  3. Add stubs
+  4. Generate multi-homed stubs
+*/
+func NewInternetwork(T, Nt, S, Ns int) *Network {
+  return new(Network)
+}
+
+
 func NewRandomUniformNetwork(nodes, edges, minLatency, maxLatency int) *Network {
   // reference: http://economics.mit.edu/files/4622
   network := new(Network)
