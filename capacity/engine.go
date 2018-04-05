88ed58435e26119d10f507e97c986799244fdd1f
Alexandru Dan <dan.alex97@yahoo.com>
2018-04-05 14:30:57 +0100

    Begin refactoring engine so it is easier to use.

--- capacity/engine.go
+++ capacity/engine.go
@@ -1,16 +1,28 @@
 package capacity
 
+import (
+  "sync"
+)
+
+/* Interface. */
 type Engine interface {
   Node
 
-  Connect(Engine) Link
+  Id() string
+  Connect(string) Link
 
-  ControlSend(Engine, interface {})
+  ControlSend(string, interface {})
   ControlRecv() <-chan interface {}
 }
 
+/* Global variables */
+var engineMap = *new(map[string]Engine)
+var ctr       = 0
+var mapLock   = new(sync.RWMutex)
+
 const controlMessageCapacity int = 50
 
+/* Simple node interface structure. */
 type node struct {
   down int
   up   int
@@ -24,29 +36,50 @@ func (n *node) Down() int {
   return n.down
 }
 
+/* Implementation. */
 type TransferEngine struct {
   node
   recv chan interface {}
+  id   string
 }
 
 func NewTransferEngine(up, down int) Engine {
-  return &TransferEngine{
+  mapLock.Lock()
+  defer mapLock.Unlock()
+
+  engine := &TransferEngine{
     node{
       up,
       down,
     },
     make(chan interface {}, controlMessageCapacity),
+    string(ctr),
   }
+
+  engineMap[engine.id] = engine
+  ctr++
+
+  return engine
 }
 
-func (e *TransferEngine) Connect(node Engine) Link {
-  return NewPerfectLink(e, node)
+func (e *TransferEngine) Connect(id string) Link {
+  mapLock.RLock()
+  defer mapLock.RUnlock()
+
+  return NewPerfectLink(e, engineMap[id])
 }
 
-func (e *TransferEngine) ControlSend(engine Engine, message interface{}) {
-  engine.(*TransferEngine).recv <- message
+func (e *TransferEngine) ControlSend(id string, message interface{}) {
+  mapLock.RLock()
+  defer mapLock.RUnlock()
+
+  engineMap[id].(*TransferEngine).recv <- message
 }
 
 func (e *TransferEngine) ControlRecv() <-chan interface{} {
   return e.recv
+}
+
+func (e *TransferEngine) Id() string {
+  return e.id
 }
