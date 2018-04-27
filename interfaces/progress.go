3d4c81cd944d7ff36454bef6b64554afe9ecb2a7
Alexandru Dan <dan.alex97@yahoo.com>
2018-04-27 20:40:07 +0100

    Modified progress interface.

--- interfaces/progress.go
+++ interfaces/progress.go
@@ -2,7 +2,7 @@ package interfaces
 
 type Progress interface {
     // The function that should be called after progress has been made.
-    Progress()
+    Progress(id string)
 
     // The function that should block until we have Progressed enough.
     Advance()
