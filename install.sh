ff45cdf3c0bdfba25b03eb8bb76a7f76a2395207
Alexandru Dan <dan.alex97@yahoo.com>
2019-05-13 14:44:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,27 @@
 if [ -z "$GOPATH" ]
 then
       echo "\$GOPATH was not set. Please set it before running the installer."
-	  exit 1
+      exit 1
 fi
 
-SPEER=github.com/danalex97/Speer
+mkdir -p $GOPATH/src/danalex97
 
-# Download Speer
-go get -d $SPEER
+pushd $GOPATH/src >> /usr/null
+if [ -z "$(ls -A Speer)" ]; then
+    rm -rf Speer
+    git clone https://github.com/danalex97/Speer.git
+else
+    pushd Speer >> /usr/null
+    git pull
+    popd >> /usr/null
+fi
 
-pushd $GOPATH/src/$SPEER >> /usr/null
+pushd Speer >> /usr/null
 
 chmod +x speer.sh
 sudo cp speer.sh /usr/bin/speer
 echo "Speer installed successfully."
+
+popd >> /usr/null
 
 popd >> /usr/null
